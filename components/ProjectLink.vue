405b962a807789cd11e67212308001175a08226a
Nick Frost <nickfrost.us@gmail.com>
2020-05-30 23:15:48 -0400

    initial project link component

---
+++ components/ProjectLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <nuxt-link :to="project.uid">
+    <div>{{ project.data.title[0].text }}</div>
+    <div>{{ project.data.client.data.name}}</div>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    props: {
+      project: Object
+    },
+    mounted() {
+      console.log(this.project);
+    }
+  })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
     <h1 class="sr-only">Portfolio</h1>
+    <project-link v-for="project in projects" :key="project.id" :project="project"/>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import ProjectLink from "~/components/ProjectLink.vue";
 
   export default Vue.extend({
-    components: {}
+    components: {
+      ProjectLink
+    },
+    // @ts-ignore
+    async asyncData({$prismic, error}) {
+      try {
+        const projects = (await $prismic.api.query(
+          $prismic.predicates.at('document.type', 'project'),
+          {
+            fetch: ['project.title', 'project.client', 'project.technologies', 'project.images'],
+            fetchLinks: ['client.name']
+          }
+        )).results
+        return {
+          projects
+        }
+      } catch (e) {
+        error({statusCode: 404, message: 'Page not found'})
+      }
+    },
   })
 </script>
 
